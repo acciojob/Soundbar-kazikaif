d0b91359d4690376de4afc20359f800ab6a27dee
kazikaif <130026107+kazikaif@users.noreply.github.com>
2025-02-22 16:34:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        stopAllSounds();
+        const sound = new Audio(`./sounds/${button.dataset.sound}.mp3`);
+        sound.play();
+        button.currentSound = sound;
+    });
+});
+
+stopButton.addEventListener('click', stopAllSounds);
+
+function stopAllSounds() {
+    buttons.forEach(button => {
+        if (button.currentSound) {
+            button.currentSound.pause();
+            button.currentSound.currentTime = 0;
+        }
+    });
+}
